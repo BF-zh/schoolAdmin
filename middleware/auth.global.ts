74c06923ff3f4deb7103fb5e622ade053ab728a5
北风 <2581807417@qq.com>
2023-07-31 20:15:22 +0800

    修改了一下文件

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,8 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const auth = useState('auth')
-
-  if (to.params.id === '1')
-    return abortNavigation()
-  if (!to.path.toString().startsWith('/wecome') || to.path === '/wecome')
-    return navigateTo('/wecome/login')
+  const path = to.path.replace(/\/+$/, '') // Remove trailing slashes
+  if (to.name !== 'login' && !path.startsWith('/welcome'))
+    return navigateTo('/welcome/login')
 })
