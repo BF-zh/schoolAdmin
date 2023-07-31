56106dfb80bdcbd650db15f0779800a97878f7bc
北风 <2581807417@qq.com>
2023-07-31 16:41:39 +0800

    修改了一点点注释

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const fullpath = to.fullPath.replace(/\/+$/, '') // Remove trailing slashes
+  const fullpath = to.fullPath.replace(/\/+$/, '') // 删除路径最后的 /
   if (!fullpath.startsWith('/welcome') && to.name !== 'login')
     return navigateTo('/welcome/login')
 })
