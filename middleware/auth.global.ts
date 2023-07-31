dfb4070e4c3bb27bd79dc75035a30d79d8523b8f
BF-zh <2581807417@qq.com>
2023-07-31 14:15:03 +0800

    修改了权限控制

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,6 +1,5 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const auth = useState('auth')
-
   if (to.params.id === '1')
     return abortNavigation()
   if (!to.path.toString().startsWith('/wecome') || to.path === '/wecome')
