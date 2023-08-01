628b0b3ce62665b5fd292902219d68b8ca677ff7
TT <2505780596@qq.com>
2023-08-01 14:24:08 +0800

    新增了三个页面

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,5 +1,9 @@
 export default defineNuxtRouteMiddleware((to, from) => {
   const path = to.path.replace(/\/+$/, '') // Remove trailing slashes
-  if (path !== '/welcome/login' && !path.startsWith('/welcome'))
+  console.log(path);
+  
+  if(path === '/welcome/login' )
+    return
+  if (!path.startsWith('/welcome'))
     return navigateTo('/welcome/login')
 })
